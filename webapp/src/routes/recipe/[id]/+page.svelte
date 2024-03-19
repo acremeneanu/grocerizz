3ede05df5dd1c58236293c61f75de56e3fa1e59e
acremeneanu <alex.cremeneanu@yahoo.com>
2024-03-20 01:49:33 +0200

    await for data in recipe page

--- webapp/src/routes/recipe/[id]/+page.svelte
+++ webapp/src/routes/recipe/[id]/+page.svelte
@@ -2,8 +2,12 @@
 	export let data;
 </script>
 
-<a href="\"> Go back</a>
-<div>
-	<h1>{data.attributes.title}</h1>
-	<h2>{data.attributes.description}</h2>
-</div>
+{#await data}
+	<p>loading</p>
+{:then}
+	<a href="\"> Go back</a>
+	<div>
+		<h1>{data.attributes.title}</h1>
+		<h2>{data.attributes.description}</h2>
+	</div>
+{/await}
